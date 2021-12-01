bf7128dcdadc14b6aec888b5c4dd1c5d7e969766
enforcer007 <akhilgodvsdemon@gmail.com>
2021-12-01 21:50:08 +0530

    Formatted

--- src/day1.rs
+++ src/day1.rs
@@ -6,7 +6,10 @@ pub fn run() {
     let firststar_ans = first_star();
     let secondstar_ans = second_star();
 
-    println!("firstStar --> {}, secondStar --> {}", firststar_ans,secondstar_ans);
+    println!(
+        "firstStar --> {}, secondStar --> {}",
+        firststar_ans, secondstar_ans
+    );
 }
 
 fn read_numbers_from_file() -> Vec<i32> {
@@ -19,7 +22,7 @@ fn read_numbers_from_file() -> Vec<i32> {
     return levels;
 }
 
-fn get_incs(levels:Vec<i32>)->i32{
+fn get_incs(levels: Vec<i32>) -> i32 {
     let inc_depths: i32 = levels[1..]
         .iter()
         .zip(levels[..levels.len() - 1].iter())
@@ -36,11 +39,11 @@ fn first_star() -> i32 {
 
 fn second_star() -> i32 {
     let levels = read_numbers_from_file();
-    let composite_levels:Vec<_> = levels[..levels.len() - 2]
+    let composite_levels: Vec<_> = levels[..levels.len() - 2]
         .iter()
         .zip(levels[1..levels.len() - 1].iter())
         .zip(levels[2..].iter())
-        .map(|((x,y),z)| x+y+z)
+        .map(|((x, y), z)| x + y + z)
         .collect();
     let inc_depths = get_incs(composite_levels);
     return inc_depths;
